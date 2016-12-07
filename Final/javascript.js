51990cbd698a5f18e8688e2be864ed27d4ecbe6c
jameel wright <j.shermaaan@gmail.com>
2016-12-06 19:22:14 -0500

    Revert "final"
    This reverts commit e1dde8b745a3f1c980464ad055755f65fa9cf5bc.

---
+++ Final/javascript.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+function showDiv() {
+   document.getElementById('more-news').style.display = "block";
+}
---
+++ Final/script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+function showNov(button) {
+    document.getElementById('more-news-1').style.display = "block";
+	document.getElementById('button-1').style.display = "none";
+}
+
+function showOct(button) {
+	document.getElementById('more-news-2').style.display = "block";
+	document.getElementById('button-2').style.display = "none";
+}
